75e2798a987c124b15b9538d1e3bce8cec6dd925
Marc Muchnick <mdmuchnick@gmail.com>
2026-09-02 19:29:53 +0000

    Set production site URL to Netlify domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from "astro/config";
 
 export default defineConfig({
-  site: "https://mffl.example.com",
+  site: "https://mffl.netlify.app",
   build: { format: "directory" },
 });
